0764094ac1ffde46da28ddf55189ea62ec2304e3
New <momarhughes@hotmail.com>
2023-02-06 11:11:53 +1000

    feat: authentication system
    added a dual-purpose login/register page and guarded JournalPage from unauthenticated Users.
    included menu with logout button.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorized = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
   {
     path: 'journal',
     loadChildren: () =>
       import('./journal/journal.module').then((m) => m.JournalPageModule),
+    ...canActivate(redirectUnauthorized),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./auth/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: '',
